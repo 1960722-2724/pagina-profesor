48349989976681e90c30eb01de4f6dc300485d17
andres david valencia giraldo <146265513+1960722-2724@users.noreply.github.com>
2025-04-30 22:58:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ document.getElementById('formulario-contacto').addEventListener('submit', functi
   e.preventDefault();
   alert('Gracias por tu mensaje. Me comunicaré pronto contigo.');
   this.reset(); // Limpiar el formulario
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5692239976330224"
+     crossorigin="anonymous"></script>
+<!-- bloque prueba -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5692239976330224"
+     data-ad-slot="7158599391"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 });
